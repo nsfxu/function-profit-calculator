0df05d71990b2aaddccf068587fc1920bce9be55
Giusier F. <giusier@whitewall.dev>
2025-01-19 14:26:41 -0300

    added adjustments

--- profit-calculator.go
+++ profit-calculator.go
@@ -7,20 +7,27 @@ func main() {
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	revenue = printAndScan("Revenue: ")
+	expenses = printAndScan("Expenses: ")
+	taxRate = printAndScan("Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := ebtProfitAndRatioCalculation(revenue, expenses, taxRate)
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+}
+
+func printAndScan(text string) (value float64) {
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return value
+}
+
+func ebtProfitAndRatioCalculation(revenue float64, expenses float64, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+
+	return ebt, profit, ratio
 }
